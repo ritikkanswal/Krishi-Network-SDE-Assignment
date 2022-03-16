0bea7c1e6fa24ce1fbd051bca9798841c3860bec
Ritik Kanswal <38499558+ritikkanswal@users.noreply.github.com>
2022-03-16 19:32:25 +0530

    "

--- posts/__init__.py
+++ posts/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from decouple import config
 db = SQLAlchemy()
 
 def create_app():
     app=Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:test123@localhost/postgres'
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
