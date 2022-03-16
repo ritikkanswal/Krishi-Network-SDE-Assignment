a68f03ae3805e61566eaa176e5ac2c5dfea7e364
Ritik Kanswal <38499558+ritikkanswal@users.noreply.github.com>
2022-03-16 19:11:12 +0530

    procfile added

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
-from posts import create_app
+from posts import create_app as a1
+from weather import create_app as a2
 
-app = create_app()
+app1 = a1()
+app2 = a2()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app1.run(debug=True)
+    # app2.run(debug=True)
