5c33a19177b5f1c37ad8966085f11990f7213743
Ritik Kanswal <38499558+ritikkanswal@users.noreply.github.com>
2022-03-16 19:58:44 +0530

    /

--- posts/__init__.py
+++ posts/__init__.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 
 def create_app():
     app=Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URI')
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
